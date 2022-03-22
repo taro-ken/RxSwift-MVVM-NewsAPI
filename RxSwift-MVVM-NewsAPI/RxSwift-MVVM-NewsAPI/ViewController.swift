6584e325f5bb9b0c7975a39211799383d8ffeccb
kentaro kimoto <exiler2411k@gmail.com>
2022-03-22 21:50:09 +0900

    init

---
+++ RxSwift-MVVM-NewsAPI/RxSwift-MVVM-NewsAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwift-MVVM-NewsAPI
+//
+//  Created by 木元健太郎 on 2022/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
